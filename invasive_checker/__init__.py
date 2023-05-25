8e5168387d08ae8356b75ca1cdcb0f71629d18a9
JotaFan <jotaflame@gmail.com>
2023-05-25 11:10:30 +0200

    refactor: add actions, gh and pre-commit, refactor calls

--- invasive_checker/__init__.py
+++ invasive_checker/__init__.py
@@ -1,5 +1,6 @@
 """Top-level package for invasive_checker."""
+import importlib.metadata
 
 __author__ = """Rory Meyer"""
-__email__ = 'rory.meyer@vliz.be'
-__version__ = '0.1.0'
+__email__ = "rory.meyer@vliz.be"
+__version__ = importlib.metadata.version("invasive_checker")
